fa99542928a6e223c23617b7ed359453a9dacc2f
Cristian Bucur <cristian.bucur@dedalus.eu>
2022-10-10 17:17:56 +0100

    created Route for pages

--- src/Components/MovieList/MovieListContainer.jsx
+++ src/Components/MovieList/MovieListContainer.jsx
@@ -8,48 +8,37 @@ import Upcoming from "../Upcoming";
 import TopRated from "../TopRated";
 import Popular from "../Popular";
 import Search from "../SearchPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 export default function MovieListContainer() {
   const [nav, setNav] = useState("upcoming"); ////using state to navigate between page,
   //in out example it is easier than using Nav component, with Upcoming set as the "Homepage"
-
+  const [favArr, setFavArr] = useState([]);
   return (
-    <>
+    <Router>
       <ButtonContainer>
-        <button
-          onClick={() => {
-            setNav("search");
-          }}
-        >
-          Search For movies
+        <button>
+          <Link to="/Upcoming">Upcoming</Link>
         </button>
-        <button
-          onClick={() => {
-            setNav("upcoming");
-          }}
-        >
-          Upcoming
+        <button>
+          <Link to="/top_rated">Top Rated</Link>
         </button>
-        <button
-          onClick={() => {
-            setNav("top_rated");
-          }}
-        >
-          Top Rated
+        <button>
+          <Link to="/popular">Popular</Link>
         </button>
-        <button
-          onClick={() => {
-            setNav("popular");
-          }}
-        >
-          Popular
+        <button>
+          <Link to="/search">Search</Link>
         </button>
       </ButtonContainer>
+      <div>{favArr.length}</div>
       <MovieContainer>
-        {nav === "upcoming" ? <Upcoming /> : " "}
-        {nav === "top_rated" ? <TopRated /> : " "}
-        {nav === "popular" ? <Popular /> : " "}
-        {nav === "search" ? <Search /> : " "}
+        <Routes>
+          <Route path="/Upcoming" element={<Upcoming count={favArr} />} />
+          <Route path="/top_rated" element={<TopRated />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
       </MovieContainer>
-    </>
+    </Router>
   );
 }
