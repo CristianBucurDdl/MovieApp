4aa8d2e6eafe4811c15cf04fedc8408ab2b5bde1
Cristian Bucur <cristian.bucur@dedalus.eu>
2024-06-26 14:39:43 +0100

    fixing issue to deply failiure

--- src/Components/pageComponents/MovieContainer.jsx
+++ src/Components/pageComponents/MovieContainer.jsx
@@ -43,7 +43,7 @@ export default function MovieContainer({ each, handleFav, favArr }) {
       <div className="title">
       <div className="title">
       <div className="title">
-      <div className="title">
+   
       <div className="title"> */}
         <h3 key={each + 1}>{each.title}</h3>
       </div>
